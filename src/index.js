22e9fa25e25c0ea9ec6a6768efd703371532512d
GalinaSolopiy <sharicbo@gmail.com>
2018-02-21 22:17:38 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 module.exports = function warmup(temperature) {
   // your implementation here
     //first commit
+    var tempPharyngeit = temperature * 1.8 + 32 ;
+    return tempPharyngeit;
 
 };
